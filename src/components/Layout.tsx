8d033188ff6eeeb46deb40e6a0e6171c67bddc79
Heraldo Sonhi <heraldosonhi@gmail.com>
2025-10-14 14:18:06 +0100

    fix: rename navbar component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
     import { Outlet } from 'react-router-dom';
-    import Navbar from './NavBar';
+    import Navbar from './NavBar'; // Import the Navbar component
 
     function Layout() {
       return (
