415b1e6229f09c9141a6c8c3abd2dc8b3ff7ee87
Heraldo Sonhi <heraldosonhi@gmail.com>
2025-10-16 20:53:05 +0100

    fix: privacy page error

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -1,4 +1,6 @@
 const Privacy = () => {
-        
+          return (
+             <section className="bg-white">Termos de uso e Políticas de Privacidade</section>
+          )
 };
 export default Privacy;
