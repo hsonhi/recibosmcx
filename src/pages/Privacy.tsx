4c08efcffbc5f17b0ce53b7fec8ca02df0a71b83
Heraldo Sonhi <heraldosonhi@gmail.com>
2025-10-17 22:09:34 +0100

    fix: update privacy page

--- src/pages/Privacy.tsx
+++ src/pages/Privacy.tsx
@@ -1,6 +1,10 @@
 const Privacy = () => {
           return (
-             <section className="bg-white">Termos de uso e Políticas de Privacidade</section>
+              <section className="bg-white">
+  <div className="max-w-[920px] my-6 mx-auto p-5  flex flex-col">
+       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900">Termos de uso e Políticas de Privacidade</h2>
+      </div>
+      </section>
           )
 };
 export default Privacy;
